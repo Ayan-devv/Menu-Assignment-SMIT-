88790bfb12b6cdd280abe40cf14e221a13ac6f5d
Ayan <160330735+Ayan-devv@users.noreply.github.com>
2024-12-23 02:20:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+function snacks() {
+  var snacks = document.querySelector(".snacksMain");
+  var drinks = document.querySelector(".drinksMain");
+  var lunch = document.querySelector(".lunchMain");
+  var dinner = document.querySelector(".dinnerMain");
+  var btn4 = document.querySelector(".btn4");
+  var btn3 = document.querySelector(".btn3");
+  var btn2 = document.querySelector(".btn2");
+  var btn1 = document.querySelector(".btn1");
+
+  btn4.style.backgroundColor = "transparent";
+  btn4.style.color = "black";
+  btn3.style.backgroundColor = "transparent";
+  btn3.style.color = "black";
+  btn2.style.backgroundColor = "transparent";
+  btn2.style.color = "black";
+  btn1.style.backgroundColor = "purple";
+  btn1.style.color = "white";
+
+  snacks.style.display = "block";
+  drinks.style.display = "none";
+  lunch.style.display = "none";
+  dinner.style.display = "none";
+}
+function drinks() {
+  var snacks = document.querySelector(".snacksMain");
+  var drinks = document.querySelector(".drinksMain");
+  var lunch = document.querySelector(".lunchMain");
+  var dinner = document.querySelector(".dinnerMain");
+  var btn4 = document.querySelector(".btn4");
+  var btn3 = document.querySelector(".btn3");
+  var btn2 = document.querySelector(".btn2");
+  var btn1 = document.querySelector(".btn1");
+
+  btn4.style.backgroundColor = "transparent";
+  btn4.style.color = "black";
+  btn3.style.backgroundColor = "transparent";
+  btn3.style.color = "black";
+  btn2.style.backgroundColor = "purple";
+  btn2.style.color = "white";
+  btn1.style.backgroundColor = "transparent";
+  btn1.style.color = "black";
+
+  snacks.style.display = "none";
+  drinks.style.display = "block";
+  lunch.style.display = "none";
+  dinner.style.display = "none";
+}
+
+function lunch() {
+  var snacks = document.querySelector(".snacksMain");
+  var drinks = document.querySelector(".drinksMain");
+  var lunch = document.querySelector(".lunchMain");
+  var dinner = document.querySelector(".dinnerMain");
+  var btn4 = document.querySelector(".btn4");
+  var btn3 = document.querySelector(".btn3");
+  var btn2 = document.querySelector(".btn2");
+  var btn1 = document.querySelector(".btn1");
+
+  btn4.style.backgroundColor = "transparent";
+  btn4.style.color = "black";
+  btn3.style.backgroundColor = "purple";
+  btn3.style.color = "white";
+  btn2.style.backgroundColor = "transparent";
+  btn2.style.color = "black";
+  btn1.style.backgroundColor = "transparent";
+  btn1.style.color = "black";
+
+  snacks.style.display = "none";
+  drinks.style.display = "none";
+  lunch.style.display = "block";
+  dinner.style.display = "none";
+}
+function dinner() {
+  var snacks = document.querySelector(".snacksMain");
+  var drinks = document.querySelector(".drinksMain");
+  var lunch = document.querySelector(".lunchMain");
+  var dinner = document.querySelector(".dinnerMain");
+  var btn4 = document.querySelector(".btn4");
+  var btn3 = document.querySelector(".btn3");
+  var btn2 = document.querySelector(".btn2");
+  var btn1 = document.querySelector(".btn1");
+
+  btn4.style.backgroundColor = "purple";
+  btn4.style.color = "white";
+  btn3.style.backgroundColor = "transparent";
+  btn3.style.color = "black";
+  btn2.style.backgroundColor = "transparent";
+  btn2.style.color = "black";
+  btn1.style.backgroundColor = "transparent";
+  btn1.style.color = "black";
+
+  snacks.style.display = "none";
+  drinks.style.display = "none";
+  lunch.style.display = "none";
+  dinner.style.display = "block";
+}
